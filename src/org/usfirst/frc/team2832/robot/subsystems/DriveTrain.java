a3e7b123fcb183881c6b1207d39515c0f966effb
Justin Marentette <justinmarentette11@gmail.com>
2018-01-13 14:40:41 -0500

    Motor declarations

--- src/org/usfirst/frc/team2832/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2832/robot/subsystems/DriveTrain.java
@@ -3,6 +3,9 @@ package org.usfirst.frc.team2832.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;;
 
 /**
  *
@@ -12,11 +15,33 @@ public class DriveTrain extends Subsystem {
 	final int SOLENOID_FORWARD_CHANNEL = 9001; //It's over 9000!
 	final int SOLENOID_REVERSE_CHANNEL = 9002;
 	
+	final int DRIVE_MOTER_FL = 9003;
+	final int DRIVE_MOTER_FR = 9004;
+	final int DRIVE_MOTER_BL = 9005;
+	final int DRIVE_MOTER_BR = 9006;
+	
 	private DoubleSolenoid transmission;
+	
+	private DifferentialDrive drive;
+	
+	private SpeedControllerGroup leftMoters;
+	private SpeedControllerGroup rightMoters;
+	
+	private WPI_TalonSRX talonFL;
+	private WPI_TalonSRX talonFR;
+	private WPI_TalonSRX talonBL;
+	private WPI_TalonSRX talonBR;
 	
 	public DriveTrain() {
 		super();
 		transmission = new DoubleSolenoid(SOLENOID_FORWARD_CHANNEL, SOLENOID_REVERSE_CHANNEL);
+		talonFL = new WPI_TalonSRX(DRIVE_MOTER_FL);
+		talonFR = new WPI_TalonSRX(DRIVE_MOTER_FR);
+		talonBL = new WPI_TalonSRX(DRIVE_MOTER_BL);
+		talonBR = new WPI_TalonSRX(DRIVE_MOTER_BR);
+		leftMoters = new SpeedControllerGroup(talonFL, talonBL);
+		rightMoters = new SpeedControllerGroup(talonFR, talonBR);
+		drive = new DifferentialDrive(leftMoters, rightMoters);
 	}
 
     public void initDefaultCommand() {
