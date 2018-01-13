0ef891a140085d74d29e319ba6e192566c0c6b97
Justin Marentette <justinmarentette11@gmail.com>
2018-01-13 13:49:44 -0500

    Transmission

--- src/org/usfirst/frc/team2832/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2832/robot/subsystems/DriveTrain.java
@@ -2,13 +2,22 @@ package org.usfirst.frc.team2832.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 /**
  *
  */
 public class DriveTrain extends Subsystem {
 
+	final int SOLENOID_FORWARD_CHANNEL = 9001; //It's over 9000!
+	final int SOLENOID_REVERSE_CHANNEL = 9002;
+	
 	private DoubleSolenoid transmission;
+	
+	public DriveTrain() {
+		super();
+		transmission = new DoubleSolenoid(SOLENOID_FORWARD_CHANNEL, SOLENOID_REVERSE_CHANNEL);
+	}
 
     public void initDefaultCommand() {
         
@@ -17,6 +26,23 @@ public class DriveTrain extends Subsystem {
     @Override
     public void periodic() {
         
+    }
+    
+    public void shift(GEAR gear) {
+    	transmission.set(gear.getValue());
+    }
+    
+    public enum GEAR {
+    	HIGH(Value.kReverse), 
+    	LOW(Value.kForward);
+    	
+    	private Value value;
+    	GEAR(Value value) {
+    		this.value = value;
+    	}
+    	public Value getValue() {
+    		return value;
+    	}
     }
 }
 
