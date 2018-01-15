a5d2de2aa2611c6923fc4e4edec27e1f43196842
Justin Marentette <justinmarentette11@gmail.com>
2018-01-15 18:16:01 -0500

    Added controller stuff with 3 options and fixed a few little things.

--- src/org/usfirst/frc/team2832/robot/Robot.java
+++ src/org/usfirst/frc/team2832/robot/Robot.java
@@ -16,33 +16,23 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends TimedRobot {
 
-	private DriveTrain driveTrain = new DriveTrain();
+	private DriveTrain driveTrain;
 	private SendableChooser chooser;
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	private Controls controls;
+	
 	@Override
 	public void robotInit() {
 		chooser = new SendableChooser();
 		chooser.addDefault("AutonTest", AutonTest.class);
 		SmartDashboard.putData("Autonomous mode chooser", chooser);
+		
+		controls = new Controls();
+		
+		driveTrain = new DriveTrain(controls);
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
 	@Override
 	public void disabledInit() {
 
@@ -53,39 +43,17 @@ public class Robot extends TimedRobot {
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
 	@Override
 	public void autonomousInit() {
-		Scheduler.getInstance().add(new AutonTest(driveTrain));
+		new AutonTest(driveTrain).start();
 		
 		Command command = (Command)chooser.getSelected();
-		command.start();	
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
-
-		// schedule the autonomous command (example)
-		
-		//YAY - Calvin
-	
+		if(command != null)
+			command.start();
+		else
+			System.err.println("Command chooser returned null");
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
@@ -93,23 +61,15 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
+		//Scheduler.getInstance().removeAll();
 	}
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+		controls.update();
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
 	@Override
 	public void testPeriodic() {
 	}
--- src/org/usfirst/frc/team2832/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2832/robot/subsystems/DriveTrain.java
@@ -1,17 +1,27 @@
 package org.usfirst.frc.team2832.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team2832.robot.Button;
+import org.usfirst.frc.team2832.robot.Controls;
+import org.usfirst.frc.team2832.robot.Controls.Buttons;
+import org.usfirst.frc.team2832.robot.Controls.Controllers;
+import org.usfirst.frc.team2832.robot.commands.ArcadeDrive;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;;
 
 /**
  *
  */
 public class DriveTrain extends Subsystem {
 
+	//Should these constants be static? Don't know of an example use, but could exist.
 	final int SOLENOID_FORWARD_CHANNEL = 9001; //It's over 9000!
 	final int SOLENOID_REVERSE_CHANNEL = 9002;
 	
@@ -20,19 +30,17 @@ public class DriveTrain extends Subsystem {
 	final int DRIVE_MOTER_BL = 17;
 	final int DRIVE_MOTER_BR = 2;
 	
+	final Button SHIFT_BUTTON = new Button(Controllers.CONTROLLER_MAIN, Buttons.Y);
+	
 	private DoubleSolenoid transmission;
-	
 	private DifferentialDrive drive;
+	private SpeedControllerGroup leftMoters, rightMoters;	
+	private WPI_TalonSRX talonFL, talonFR, talonBL, talonBR;
+	private Encoder encoderLeft, encoderRight;
 	
-	private SpeedControllerGroup leftMoters;
-	private SpeedControllerGroup rightMoters;
+	private Controls controls;
 	
-	private WPI_TalonSRX talonFL;
-	private WPI_TalonSRX talonFR;
-	private WPI_TalonSRX talonBL;
-	private WPI_TalonSRX talonBR;
-	
-	public DriveTrain() {
+	public DriveTrain(Controls controls) {
 		super();
 		transmission = new DoubleSolenoid(SOLENOID_FORWARD_CHANNEL, SOLENOID_REVERSE_CHANNEL);
 		talonFL = new WPI_TalonSRX(DRIVE_MOTER_FL);
@@ -42,19 +50,28 @@ public class DriveTrain extends Subsystem {
 		leftMoters = new SpeedControllerGroup(talonFL, talonBL);
 		rightMoters = new SpeedControllerGroup(talonFR, talonBR);
 		drive = new DifferentialDrive(leftMoters, rightMoters);
+		//encoderLeft = new Encoder();
 	}
 
     public void initDefaultCommand() {
-        
+        setDefaultCommand(new ArcadeDrive(this, controls));
     }
     
     @Override
     public void periodic() {
-        
+        if(controls.getButtonPressed(SHIFT_BUTTON.getController(), SHIFT_BUTTON.getButton())) {
+        	toggleShift();
+        	controls.setRumble(Controllers.CONTROLLER_MAIN, RumbleType.kLeftRumble, 0.5d, 0.5d);
+        	controls.setRumble(Controllers.CONTROLLER_MAIN, RumbleType.kRightRumble, 0.5d, 0.5d);
+        }
     }
     
     public void shift(GEAR gear) {
     	transmission.set(gear.getValue());
+    }
+    
+    public void toggleShift() {
+    	transmission.set(transmission.get().equals(GEAR.HIGH.getValue()) ? GEAR.LOW.getValue(): GEAR.HIGH.getValue());
     }
     
     public DifferentialDrive getDrive() {
