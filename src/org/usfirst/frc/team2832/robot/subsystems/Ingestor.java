8931e09532ff3efa322583f415393e2de9034ced
CalvinTheRed <calvin@brianandkathi.com>
2018-01-31 18:56:46 -0500

    Calvin thought about his life and fixed all of his problems on his computer. Ingestor subsystem is updated now.

--- src/org/usfirst/frc/team2832/robot/subsystems/Ingestor.java
+++ src/org/usfirst/frc/team2832/robot/subsystems/Ingestor.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team2832.robot.Controls.Buttons;
 import org.usfirst.frc.team2832.robot.Controls.Controllers;
 import org.usfirst.frc.team2832.robot.Robot;
 import org.usfirst.frc.team2832.robot.ButtonMapping;
+import org.usfirst.frc.team2832.robot.Controls;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
@@ -12,14 +13,15 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Ingestor extends Subsystem {
 	
-	final static int INGESTOR_L = 9000; // Fix these
+	final static int INGESTOR_L = 9000; // fix these
 	final static int INGESTOR_R = 9001;
-	final static int FORWARD_CHANNEL = 5; // check these
-	final static int REVERSE_CHANNEL = 6;
+	final static int FORWARD_CHANNEL = 4; // assign these to pneumatics
+	final static int REVERSE_CHANNEL = 5;
 	
 	private TalonSRX talonL;
 	private TalonSRX talonR;
@@ -75,16 +77,34 @@ public class Ingestor extends Subsystem {
 	
 	public void setBrakeMode(boolean mode) {
     	NeutralMode brakeMode;
-    	if(mode) 
+    	if (mode) {
     		brakeMode = NeutralMode.Brake;
-    	else
+    	} else {
     		brakeMode = NeutralMode.Coast;
+    	}
        	talonL.setNeutralMode(brakeMode);
     	talonR.setNeutralMode(brakeMode);
     }
 	
 	public void periodic () {
+		double tLeft = Robot.controls.getTrigger(Controls.Controllers.CONTROLLER_MAIN, Hand.kLeft); // intake
+		double tRight = Robot.controls.getTrigger(Controls.Controllers.CONTROLLER_MAIN, Hand.kRight); // expel
 		
+		if (  Robot.controls.getButtonPressed(Robot.ingestor.TOGGLE_TILT_0.getController(), 
+											  Robot.ingestor.TOGGLE_TILT_0.getButton()) 
+		   || Robot.controls.getButtonPressed(Robot.ingestor.TOGGLE_TILT_1.getController(), 
+											  Robot.ingestor.TOGGLE_TILT_1.getButton()) 
+		   )
+		{
+			System.out.println("toggle tilt");
+			Robot.ingestor.toggleTilt();
+		} else if (tLeft > 0) { // fix this [-1,1]
+			setMotorSpeed(tLeft); // check sign and include motor speed conversion
+		} else if (tRight > 0) { // fix this [-1,1]
+			setMotorSpeed(tRight); // check sign and include motor speed conversion
+		} else {
+			stopMotors();
+		}
 	}
 
 }
