d0d56879ff0b3eb3655771ed3fae9d31ad4de91d
Justin Marentette <justinmarentette11@gmail.com>
2018-02-05 17:31:40 -0500

    Well, this was kind of pathetic... Let's never look at this again.

--- src/org/usfirst/frc/team2832/robot/DrivingTest.java
+++ src/org/usfirst/frc/team2832/robot/DrivingTest.java
@@ -1,11 +1,12 @@
 package org.usfirst.frc.team2832.robot;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class DrivingTest implements KeyEventDispatcher {
 
-    private Robot robot;
+    public static Robot robot;
     private int speed, direction;
 
     public void init() {
@@ -14,16 +15,34 @@ public class DrivingTest implements KeyEventDispatcher {
 
     public void periodic() {
         robot.arcadeDrive(speed, direction);
+        robot.update();
     }
 
     private class Robot {
 
-        public void getVelocity() {
+        private double xVelocity, yVelocity, angle, x, y;
 
+        public double getVelocity() {
+            return (xVelocity + yVelocity) / 2;
+        }
+
+        public void update() {
+            if(Math.abs(xVelocity) < 0.1)
+                xVelocity = 0;
+            if(Math.abs(yVelocity) < 0.1)
+                xVelocity = 0;
+            if(xVelocity != 0)
+                xVelocity -= Math.signum(xVelocity) * 1;
+            if(yVelocity != 0)
+                yVelocity -= Math.signum(yVelocity) * 1;
+            x += xVelocity;
+            y += yVelocity;
         }
 
         public void arcadeDrive(double speed, double rotation) {
-
+            angle += rotation / 100d;
+            xVelocity += speed * Math.cos(angle);
+            yVelocity += speed * Math.sin(angle);
         }
     }
 
@@ -32,6 +51,8 @@ public class DrivingTest implements KeyEventDispatcher {
         synchronized (DrivingTest.class) {
             switch (ke.getID()) {
                 case KeyEvent.KEY_PRESSED:
+                    if (ke.getKeyCode() == KeyEvent.VK_ESCAPE)
+                        System.exit(0);
                     if (ke.getKeyCode() == KeyEvent.VK_W)
                         speed++;
                     if (ke.getKeyCode() == KeyEvent.VK_S)
@@ -61,8 +82,40 @@ public class DrivingTest implements KeyEventDispatcher {
         DrivingTest test = new DrivingTest();
         KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(test);
         test.init();
+
+        JFrame frame = new JFrame("Skidesign");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        gd.setFullScreenWindow(frame);
+        frame.add(new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setColor(Color.BLACK);
+                g2d.drawRect(10, 10, frame.getWidth() - 40, frame.getHeight() - 60);
+                g2d.setColor(Color.cyan);
+                g2d.drawOval((int)robot.x, (int)robot.y, 30, 30);
+                g2d.setColor(Color.BLACK);
+                g2d.drawString("Angle: " + Double.toString(DrivingTest.robot.angle), 30, 30);
+                g2d.drawString("X Velocity: " + Double.toString(DrivingTest.robot.xVelocity), 30, 45);
+                g2d.drawString("Y Velocity: " + Double.toString(DrivingTest.robot.yVelocity), 30, 60);
+                g2d.drawString("X: " + Double.toString(DrivingTest.robot.x), 30, 75);
+                g2d.drawString("Y: " + Double.toString(DrivingTest.robot.y), 30, 90);
+                g2d.drawString("Direction: " + Double.toString(test.direction), 30, 105);
+                g2d.drawString("Speed: " + Double.toString(test.speed), 30, 120);
+            }
+        });
+        frame.setVisible(true);
+
         while(true) {
             test.periodic();
+            frame.repaint();
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+
+            }
         }
     }
 }
