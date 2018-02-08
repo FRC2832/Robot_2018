fb21079210f7d48f6d82609735790198f3128c98
CalvinTheRed <calvin@brianandkathi.com>
2018-02-07 20:33:26 -0500

    Included IR sensor logic in Ingestor subsystem

--- src/org/usfirst/frc/team2832/robot/subsystems/Ingestor.java
+++ src/org/usfirst/frc/team2832/robot/subsystems/Ingestor.java
@@ -1,7 +1,5 @@
 package org.usfirst.frc.team2832.robot.subsystems;
 
-import org.usfirst.frc.team2832.robot.Controls.Buttons;
-import org.usfirst.frc.team2832.robot.Controls.Controllers;
 import org.usfirst.frc.team2832.robot.Robot;
 import org.usfirst.frc.team2832.robot.ButtonMapping;
 import org.usfirst.frc.team2832.robot.Controls;
@@ -11,6 +9,7 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
@@ -19,11 +18,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Ingestor extends Subsystem {
 	
-	final static int INGESTOR_L = 0; // fix these
+	final static int INGESTOR_L = 0;
 	final static int INGESTOR_R = 1;
-	final static int FORWARD_CHANNEL = 2; // assign these to pneumatics
+	final static int FORWARD_CHANNEL = 2;
 	final static int REVERSE_CHANNEL = 3;
+	final static int DIGITAL_PIN = 1; // get proper channel!
 	
+	private DigitalInput di;
 	private TalonSRX talonL;
 	private TalonSRX talonR;
 	private DoubleSolenoid tilt;
@@ -31,6 +32,7 @@ public class Ingestor extends Subsystem {
 	
 	public Ingestor() {
 		super();
+		di = new DigitalInput(DIGITAL_PIN);
 		talonL = new TalonSRX(INGESTOR_L);
 		talonR = new TalonSRX(INGESTOR_R);
 		tilt = new DoubleSolenoid(FORWARD_CHANNEL, REVERSE_CHANNEL);
@@ -66,8 +68,8 @@ public class Ingestor extends Subsystem {
 	}
 	
 	public void setMotorSpeed(double sped) {
-		talonL.set(ControlMode.PercentOutput,  sped);
-		talonR.set(ControlMode.PercentOutput,  sped);
+		talonL.set(ControlMode.PercentOutput, sped);
+		talonR.set(ControlMode.PercentOutput, sped);
 	}
 	
 	public void stopMotors() {
@@ -85,10 +87,20 @@ public class Ingestor extends Subsystem {
        	talonL.setNeutralMode(brakeMode);
     	talonR.setNeutralMode(brakeMode);
     }
+	/**
+	 * Reads in the value from the IR Proximity sensor.
+	 * Returns false if obstruction is detected.
+	 */
+	public boolean readDigital() {
+		return di.get();
+	}
 	
 	public void periodic () {
 		double tLeft =  Math.abs(Robot.controls.getTrigger(Controls.Controllers.CONTROLLER_MAIN, Hand.kLeft )); // intake
 		double tRight = Math.abs(Robot.controls.getTrigger(Controls.Controllers.CONTROLLER_MAIN, Hand.kRight)); // expel
+		
+		boolean digitalVal = readDigital();
+		SmartDashboard.putBoolean(Dashboard.PREFIX_PROG + "DigitalIntake Val", digitalVal);
 		
 		if (  Robot.controls.getButtonPressed(ButtonMapping.TOGGLE_TILT_0.getController(), 
 											  ButtonMapping.TOGGLE_TILT_0.getButton()) 
@@ -97,8 +109,10 @@ public class Ingestor extends Subsystem {
 		   )
 		{
 			Robot.ingestor.toggleTilt();
-		} else if (tLeft > 0.05) { // assumes [0,1] where 0 is at rest
-			setMotorSpeed(tLeft * 0.8); // max manual motor speed is 0.8
+		} else if (tLeft > 0.05) {
+			if (digitalVal) {
+				setMotorSpeed(tLeft * 0.8); // max manual motor speed is 0.8
+			}
 		} else if (tRight > 0.05) {
 			setMotorSpeed(tRight * 0.8);
 		} else {
